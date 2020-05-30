fbac70038f675a7369c3f49fcca5be412fffbda9
Ola <olawunmi263@yahoo.com>
2020-05-30 06:03:54 +0100

    Steps Defined

---
+++ src/test/java/co/uk/next/features/SearchSummerStyles.feature
@@ -0,0 +1,12 @@
+Feature: Search Summer Styles
+  As a user
+  I want to search summer styles
+  So I can view available items
+
+  Scenario: User can successfully search summer items
+
+    Given I navigate to next website
+    When I click on the homepage header
+    Then a list of summer essentials is displayed
+    And I click on any
+
---
+++ src/test/java/co/uk/next/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,28 @@
+package co.uk.next.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("I navigate to next website")
+    public void i_navigate_to_next_website() {
+
+    }
+
+    @When("I click on the header")
+    public void i_click_on_the_homepage_header() {
+
+    }
+
+    @Then("a list of summer essentials is displayed")
+    public void a_list_of_summer_essentials_is_displayed() {
+
+    }
+
+    @Then("I click on any")
+    public void i_click_on_any() {
+
+    }
+
+}
