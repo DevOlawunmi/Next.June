a4841b85fd03868b86522ac4df5f12b2928a0c3d
Ola <olawunmi263@yahoo.com>
2020-05-30 07:19:52 +0100

    Steps Defined

---
+++ src/test/java/co/uk/next/common/DriverLib.java
@@ -0,0 +1,10 @@
+package co.uk.next.common;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DriverLib {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+}
---
+++ src/test/java/co/uk/next/hook/Hook.java
@@ -0,0 +1,4 @@
+package co.uk.next.hook;
+
+public class Hook {
+}
